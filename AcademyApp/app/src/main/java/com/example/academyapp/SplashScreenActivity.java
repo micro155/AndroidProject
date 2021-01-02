41a24193c6df45b8b17ff8c09b3912374d778bcd
micro155 <qawe2006@naver.com>
2021-01-02 23:33:18 +0900

    인트로 화면 완료

--- AcademyApp/app/src/main/java/com/example/academyapp/SplashScreenActivity.java
+++ AcademyApp/app/src/main/java/com/example/academyapp/SplashScreenActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Completable;
@@ -13,10 +18,25 @@ import io.reactivex.functions.Action;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private final static int LOGIN_REQUEST_CODE = 7000;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        delaySplashScreen();
+
+
+        init();
+    }
+
+    private void init() {
+        providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(), new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        firebaseAuth = firebaseAuth.getInstance();
+        listener = myFirebaseAuth
+
     }
 
     private void delaySplashScreen() {
