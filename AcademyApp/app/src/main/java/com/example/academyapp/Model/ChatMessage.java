9c3acb802a9f3804ac910ac0fc531bee09415831
micro155 <qawe2006@naver.com>
2021-03-24 23:49:40 +0900

    채팅 기능 구현중

---
+++ AcademyApp/app/src/main/java/com/example/academyapp/Model/ChatMessage.java
@@ -0,0 +1,44 @@
+package com.example.academyapp.Model;
+
+public class ChatMessage {
+    String id, text,name,photoURL;
+
+    public ChatMessage(String id, String text, String name, String photoURL) {
+        this.id = id;
+        this.text = text;
+        this.name = name;
+        this.photoURL = photoURL;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+}
